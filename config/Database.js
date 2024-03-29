a7a139110fd1a5cee7799261db63f5301a60883c
Gaurav Singh <sumitgaurav6295@gmail.com>
2024-03-29 22:22:46 +0530

    initiated blog app

---
+++ config/Database.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+// load environment variables from a .env file into process.env
+require("dotenv").config();
+
+const connectWithDb = () => {
+  mongoose
+    .connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(console.log("db connection successfully"))
+    .catch((error) => {
+      console.log("db is facing connection issue");
+      console.error(error);
+      process.exit(1);
+    });
+};
